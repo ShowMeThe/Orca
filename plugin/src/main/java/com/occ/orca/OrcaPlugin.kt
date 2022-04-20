2f4c0600df7c27855dd801da0121d674774af505
cuvsuv <c.uvsuv@163.com>
2022-04-20 21:22:00 +0800

    maven testing

--- plugin/src/main/java/com/occ/orca/OrcaPlugin.kt
+++ plugin/src/main/java/com/occ/orca/OrcaPlugin.kt
@@ -203,7 +203,7 @@ class OrcaPlugin : Plugin<Project> {
         var result: Any? = null
         project.buildscript.configurations.forEach { config ->
             val file = config.files.find {
-                it.name.contains("orca")
+                it.path.contains("orca",true) && it.name.contains("plugin",true)
             }
             if (file != null) {
                 result = project.zipTree(file)
