5ae8a4bf2f9d679126d9b4cc4e8175df99978bf3
ken <c.uvsuv@163.com>
2022-04-20 14:18:26 +0800

    compatible with Gradle 7.0

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.test
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.orcc.app.core.CoreClient
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,12 +12,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        var lastTime = System.nanoTime()
-        var data = CoreClient.getData()
-        Log.e("MainActivity","${data} ${System.nanoTime() - lastTime}")
-        lastTime = System.nanoTime()
-        data = CoreClient.getData()
-        Log.e("MainActivity","${data} ${System.nanoTime() - lastTime}")
+
 
 
     }
