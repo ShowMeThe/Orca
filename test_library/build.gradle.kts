0e017c614b2bc8aeb5e429ce10a5b443723c26d9
ken <c.uvsuv@163.com>
2023-02-28 16:00:25 +0800

    fix Kotlin class reflect delete WhiteList

--- test_library/build.gradle.kts
+++ test_library/build.gradle.kts
@@ -53,6 +53,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 
-    implementation(project(":orca-annotation"))
-    //implementation("com.github.ShowMeThe.Orca:orca-annotation:2.3.0")
+//    implementation(project(":orca-annotation"))
+    implementation("com.github.ShowMeThe.Orca:orca-annotation:2.3.0")
 }
